85f7f8fda93c162a702434962d3e011dc15dbec6
Jaz Singh <jbsingh55@gmail.com>
2018-02-25 22:15:24 -0800

    uh woah

---
+++ src/main/java/dexter/maybach/App.java
@@ -0,0 +1,13 @@
+package dexter.maybach;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
